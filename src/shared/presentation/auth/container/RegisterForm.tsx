0786e4f5f256bf2e0bc6cde0af1c0ec50a05d2c4
tinnd <tinnd@runsystem.net>
2025-03-27 17:11:31 +0700

    sdsdsd

--- src/shared/presentation/auth/container/RegisterForm.tsx
+++ src/shared/presentation/auth/container/RegisterForm.tsx
@@ -42,8 +42,8 @@ export default function RegisterForm() {
   const onSubmit = async (data: RegisterSchema) => {
     const isValid = await trigger()
     if (!isValid) return
-    // const response = await axios.post('/auth/sign-up', data)
-    const response = await axios.post('/api/auth/register', data)
+    const response = await apiClient.post('/auth/sign-up', data)
+    // const response = await axios.post('/api/auth/register', data)
     if (response.status === CODE_STATUS.CREATED) {
       reset()
       toast.success(MESSAGE_STATUS.REGISTER_SUCCESS)
