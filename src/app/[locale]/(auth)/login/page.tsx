84cbdbf00f27b24f5433da4a4569aa80294d7405
tinnd <tinnd@runsystem.net>
2025-03-27 14:17:53 +0700

    ssss

--- src/app/[locale]/(auth)/login/page.tsx
+++ src/app/[locale]/(auth)/login/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react'
 import LoginForm from '@/shared/presentation/auth/container/LoginForm'
 
 export default function Login() {
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <LoginForm />
