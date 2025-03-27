17ddc3af76309f75eb0f2d6b589de4b8bee91872
tinnd <tinnd@runsystem.net>
2025-03-27 14:59:34 +0700

    sss

--- src/shared/helpers/auth.ts
+++ src/shared/helpers/auth.ts
@@ -35,29 +35,21 @@ export interface LoginCredentials {
 }
 export const loginUser = async (credentials: LoginCredentials) => {
   console.log('credentials', credentials)
-  try {
-    const { data } = await apiClient.post('/auth/sign-in', credentials)
-    console.log('data từ api', data)
-    console.log('token từi api', data.access_token)
-    if (data.access_token) {
-      return {
-        id: data.user_id,
-        email: credentials.email,
-        accessToken: data.access_token,
-        refreshToken: data.refresh_token,
-        organizationId: data.organization_id,
-        type: data.type,
-        iat: data.iat,
-        exp: data.exp,
-      }
-    }
 
-    return null
-  } catch (error) {
-    console.log('error', error)
-    if (error instanceof Error) {
-      throw new Error(error.message || 'Login failed')
+  const { data } = await apiClient.post('/auth/sign-in', credentials)
+  console.log('data từ api', data)
+  console.log('token từi api', data.access_token)
+  if (data.access_token) {
+    return {
+      id: data.user_id,
+      email: credentials.email,
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+      organizationId: data.organization_id,
+      type: data.type,
+      iat: data.iat,
+      exp: data.exp,
     }
-    return null
   }
+  return null
 }
