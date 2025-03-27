f4b6fe96bb5fb81c8fa19ac7ebbfcfe2be3289c2
tinnd <tinnd@runsystem.net>
2025-03-27 15:28:30 +0700

    ssss

--- src/shared/helpers/auth.ts
+++ src/shared/helpers/auth.ts
@@ -34,22 +34,30 @@ export interface LoginCredentials {
   organization_code: string
 }
 export const loginUser = async (credentials: LoginCredentials) => {
-  console.log('credentials', credentials)
-
-  const { data } = await apiClient.post('/auth/sign-in', credentials)
-  console.log('data từ api', data)
-  console.log('token từi api', data.access_token)
-  if (data.access_token) {
-    return {
-      user_id: data.user_id,
-      email: credentials.email,
-      access_token: data.access_token,
-      refresh_token: data.refresh_token,
-      organization_id: data.organization_id,
-      type: data.type,
-      iat: data.iat,
-      exp: data.exp,
+  console.log('next Auth secret', process.env.NEXTAUTH_SECRET)
+  console.log('Next auth url', process.env.NEXTAUTH_URL)
+  console.log('api url', process.env.APP_API_BASE_URL)
+  try {
+    const { data } = await apiClient.post('/auth/sign-in', credentials)
+    console.log('data từ api', data)
+    console.log('token từi api', data.access_token)
+    if (data.access_token) {
+      return {
+        id: data.user_id,
+        accessToken: data.access_token,
+        refreshToken: data.refresh_token,
+        organizationId: data.organization_id,
+        type: data.type,
+        iat: data.iat,
+        exp: data.exp,
+      }
     }
+
+    return null
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message || 'Login failed')
+    }
+    return null
   }
-  return null
 }
