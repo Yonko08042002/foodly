7cdb6094cbf8f0bc3bc99d047643630c3f511169
tinnd <tinnd@runsystem.net>
2025-03-27 16:21:31 +0700

    sss

--- src/shared/helpers/auth.ts
+++ src/shared/helpers/auth.ts
@@ -39,9 +39,21 @@ export const loginUser = async (credentials: LoginCredentials) => {
   console.log('Next auth url', process.env.NEXTAUTH_URL)
   console.log('api url', process.env.APP_API_BASE_URL)
   try {
-    const { data } = await apiClient.post('/auth/sign-in', credentials)
-    console.log('data từ api', data)
-    console.log('token từi api', data.access_token)
+    const response = await fetch(`${process.env.APP_API_BASE_URL}/auth/sign-in`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    })
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`)
+    }
+
+    const data = await response.json()
+    console.log('Dữ liệu từ API:', data)
+
     if (data.access_token) {
       return {
         user_id: data.user_id,
@@ -56,12 +68,9 @@ export const loginUser = async (credentials: LoginCredentials) => {
 
     return null
   } catch (error: unknown) {
+    console.log(error)
     if (error instanceof AxiosError) {
-      console.error('Lỗi đăng nhập từ API:', error.response?.data || error.message)
-    } else if (error instanceof Error) {
-      console.error('Lỗi hệ thống:', error.message)
-    } else {
-      console.error('Lỗi không xác định:', error)
+      console.error('Lỗi đăng nhập từ API:', error.response?.data && error.message && error)
     }
     return null
   }
